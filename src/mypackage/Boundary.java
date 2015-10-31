1f443ecda3b376fcb9b1572fecd4b5507b53d860
Andrey Rozumnyi <andrey.rozumnuy@gmail.com>
2015-10-31 22:58:43 +0200

    add simple gui

--- src/mypackage/Boundary.java
+++ src/mypackage/Boundary.java
@@ -1,4 +1,10 @@
 package mypackage;
+
+import java.io.File;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+
 public class Boundary
 {
 	private static Controller controller;
@@ -6,7 +12,12 @@ public class Boundary
 	public static void main( String [] args )
 	{
 		controller = new Controller();
-		controller.execute("C:\\Users\\Dmytro\\Documents\\GitHub\\ConformanceChecker\\src\\test.xes", "C:\\Users\\Dmytro\\Documents\\GitHub\\ConformanceChecker\\src\\test.pnml");
+		JFileChooser choice = new JFileChooser();
+        choice.showOpenDialog(new JFrame());
+        File file1 = choice.getSelectedFile();
+        choice.showOpenDialog(new JFrame());
+        File file2 = choice.getSelectedFile();
+		controller.execute(file1.getAbsolutePath(), file2.getAbsolutePath());
 		System.out.println( controller.computeFitness() );
 		System.out.println( controller.computeBehavioralAppropriateness());
 		System.out.println( controller.computeStructuralAppropriateness());
